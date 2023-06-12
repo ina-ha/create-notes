69b57d69fcfac4775e75f21ec7aa937a5e8198e5
ina-ha <ina.haidava.tech@gmail.com>
2023-06-12 17:25:37 +0200

    added color switchers

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {AiOutlineClose} from 'react-icons/ai';
 import {HiOutlineDotsHorizontal} from 'react-icons/hi';
 import "../App.css";
@@ -6,9 +6,28 @@ import PopMenu from './PopMenu';
 
 function Note(props) {
   const [pop, setPop] = useState(false);
+  const [color, setColor] = useState("#6EC4D8");
+  const [topColor, setTopColor] = useState("#60b2c4");
+
+const handleYellow = () => {
+  setColor("#FFF7C0");
+  setTopColor("#f8efae");
+}
+
+const handleBlue = () => {
+  setColor("#6EC4D8");
+  setTopColor("#60b2c4");
+}
   
+const handleSalmon = () => {
+  setColor("#FA7C92");
+  setTopColor("#f76d84");
+}
 
-
+const handleGreen = () => {
+  setColor("#66AB8C");
+  setTopColor("#51a881");
+}
 
   const handleClick = () => {
     props.toRemoveNote(props.note.id);
@@ -19,9 +38,9 @@ const popMenu = () => setPop(!pop);
 
   return (
     <div className="note">
-      <PopMenu pop={pop} />
-      <div className='note-body'>
-      <div className="note-menu">
+      <PopMenu handleYellow={handleYellow} handleBlue={handleBlue} handleSalmon={handleSalmon} handleGreen={handleGreen} pop={pop} />
+      <div className='note-body' style={{backgroundColor: `${color}`}}>
+      <div className="note-menu" style={{backgroundColor: `${topColor}`}}>
       <div><HiOutlineDotsHorizontal className='icon' onClick={popMenu} /></div>
       <div><AiOutlineClose className='icon' onClick={handleClick} /></div>
       </div>
--- src/components/PopMenu.js
+++ src/components/PopMenu.js
@@ -2,15 +2,15 @@ import React from 'react'
 import {BsSquareFill} from 'react-icons/bs';
 import "../App.css";
 
-function PopMenu({pop}) {
+function PopMenu({pop, handleYellow, handleBlue, handleSalmon, handleGreen}) {
     
   return (
     <div>
 <div className={pop ? "note-popMenu-active" : "note-popMenu"}>
-        <BsSquareFill className="icon-color blue"/>
-        <BsSquareFill className="icon-color yellow"/>
-        <BsSquareFill className="icon-color salmon"/>
-        <BsSquareFill className="icon-color green"/>
+        <BsSquareFill onClick={handleBlue} className="icon-color blue"/>
+        <BsSquareFill onClick={handleYellow} className="icon-color yellow"/>
+        <BsSquareFill onClick={handleSalmon} className="icon-color salmon"/>
+        <BsSquareFill onClick={handleGreen} className="icon-color green"/>
         </div>
 
     </div>
